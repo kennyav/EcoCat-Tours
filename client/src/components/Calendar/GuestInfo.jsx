e9756d76886c946e61dfebd940549377f827918b
Koz <kenneth.averna@gmail.com>
2024-02-07 19:35:45 -0800

    styling fix to guestinfo right menu bar

--- client/src/components/Calendar/GuestInfo.jsx
+++ client/src/components/Calendar/GuestInfo.jsx
@@ -5,15 +5,15 @@ import { CheckedInIcon } from '../Icons'
 
 export default function GuestInfo(props) {
   return (
-    <div class="w-full h-[149px] pl-[26px] pt-[30px] justify-center items-center inline-flex">
-      <div class="flex-col justify-start items-start gap-[9px] inline-flex">
+    // <div class="w-full h-[149px] pl-[26px] pt-[30px] justify-center items-center inline-flex">
+      <div class="flex-col  pl-[26px] pt-[30px] justify-start items-start gap-[9px] inline-flex">
         <div class="justify-start items-start gap-1 inline-flex">
           <div class="w-[179px] flex-col justify-start items-start gap-1 inline-flex">
             <div class="justify-start items-end gap-2 inline-flex">
               <CheckedInIcon />
               <div class="text-stone-900 text-sm font-bold font-['Kumbh Sans']">John Smith</div>
             </div>
-            <div class="w-[47px] h-[15px] text-sky-700 text-xs font-medium font-['Kumbh Sans']">3 Adults</div>
+            <div class="text-sky-700 text-xs font-medium font-['Kumbh Sans']">3 Adults</div>
           </div>
           <div class="w-8 h-[18px] px-1 py-[3px] bg-orange-400 rounded-[5px] justify-center items-center gap-2.5 flex">
             <div class="text-center text-white text-[10px] font-semibold font-['Kumbh Sans']">PAID</div>
@@ -39,6 +39,6 @@ export default function GuestInfo(props) {
           <div class="w-[215px] h-[0px] border border-slate-300"></div>
         </div>
       </div>
-    </div>
+    // </div>
   )
 }
