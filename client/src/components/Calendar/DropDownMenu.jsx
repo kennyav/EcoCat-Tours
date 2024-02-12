f9f8a4ab280b5b82869e575e53e97c7c8e44727b
Koz <kenneth.averna@gmail.com>
2024-02-12 09:55:24 -0800

    extracted the dropdownmenu to be used in other components"

--- client/src/components/Calendar/DropDownMenu.jsx
+++ client/src/components/Calendar/DropDownMenu.jsx
@@ -1,5 +1,5 @@
 import { Menu, Transition } from '@headlessui/react'
-import { Fragment, useEffect, useRef, useState } from 'react'
+import { Fragment } from 'react'
 import { CalendarDownIcon } from '../Icons';
 
 export default function DropDownMenu(props) {
@@ -13,7 +13,7 @@ export default function DropDownMenu(props) {
       <Menu as="div" className="relative inline-block text-left">
          <div>
             <Menu.Button className="flex items-center gap-6 bg-transparent hover:bg-[#0E5BB5] hover:text-white py-2.5 px-8 border-2 border-[#0E5BB5] hover:border-transparent rounded-full">
-               {props.currentMonth}
+               {props.current}
                <CalendarDownIcon />
             </Menu.Button>
          </div>
@@ -29,19 +29,19 @@ export default function DropDownMenu(props) {
          >
             <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                <div className="py-1">
-                  {props.list.map((month, i) => {
+                  {props.list.map((curr, i) => {
                      return (
                         <Menu.Item key={i}>
                            {({ active }) => (
                               <span
-                                 key={month}
-                                 onClick={props.onClick(month)}
+                                 key={curr}
+                                 onClick={() => props.setCurrent(curr)}
                                  className={classNames(
                                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                                     'block px-4 py-2 text-sm'
                                  )}
                               >
-                                 {month}
+                                 {curr}
                               </span>
                            )}
                         </Menu.Item>
--- client/src/components/Calendar/Header.jsx
+++ client/src/components/Calendar/Header.jsx
@@ -1,8 +1,6 @@
-import React, { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
+import React from 'react'
 
 // icons
-import { CalendarDownIcon } from '../Icons';
 import { CalendarLeftIcon } from '../Icons';
 import { CalendarRightIcon } from '../Icons';
 
@@ -10,10 +8,6 @@ import { CalendarRightIcon } from '../Icons';
 import DropDownMenu from './DropDownMenu'
 
 const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-
-function classNames(...classes) {
-   return classes.filter(Boolean).join(' ')
-}
 
 export default function Header(props) {
    const yearsList = () => {
@@ -31,94 +25,8 @@ export default function Header(props) {
          <button className='flex items-center gap-6 bg-transparent hover:bg-[#0E5BB5] hover:border-transparent rounded-full'>
             <CalendarLeftIcon />
          </button>
-         <DropDownMenu list={MONTH_NAMES} onClick={(month) => props.setCurrentMonth(month)}/>
-         <DropDownMenu list={yearsList()} />
-
-         {/* <Menu as="div" className="relative inline-block text-left">
-            <div>
-               <Menu.Button className="flex items-center gap-6 bg-transparent hover:bg-[#0E5BB5] hover:text-white py-2.5 px-8 border-2 border-[#0E5BB5] hover:border-transparent rounded-full">
-                  {props.currentMonth}
-                  <CalendarDownIcon />
-               </Menu.Button>
-            </div>
-
-            <Transition
-               as={Fragment}
-               enter="transition ease-out duration-100"
-               enterFrom="transform opacity-0 scale-95"
-               enterTo="transform opacity-100 scale-100"
-               leave="transition ease-in duration-75"
-               leaveFrom="transform opacity-100 scale-100"
-               leaveTo="transform opacity-0 scale-95"
-            >
-               <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="py-1">
-                     {
-                        MONTH_NAMES.map((month, i) => {
-                           return (
-                              <Menu.Item key={i}>
-                                 {({ active }) => (
-                                    <span
-                                       key={month}
-                                       onClick={() => props.setCurrentMonth(month)}
-                                       className={classNames(
-                                          active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                          'block px-4 py-2 text-sm'
-                                       )}
-                                    >
-                                       {month}
-                                    </span>
-                                 )}
-                              </Menu.Item>
-                           )
-                        })
-                     }
-                  </div>
-               </Menu.Items>
-            </Transition>
-         </Menu>
-         <Menu as="div" className="relative inline-block text-left">
-            <div>
-               <Menu.Button className="flex items-center gap-6 bg-transparent hover:bg-[#0E5BB5] hover:text-white py-2.5 px-8 border-2 border-[#0E5BB5] hover:border-transparent rounded-full">
-                  {props.currentYear}
-                  <CalendarDownIcon />
-               </Menu.Button>
-            </div>
-
-            <Transition
-               as={Fragment}
-               enter="transition ease-out duration-100"
-               enterFrom="transform opacity-0 scale-95"
-               enterTo="transform opacity-100 scale-100"
-               leave="transition ease-in duration-75"
-               leaveFrom="transform opacity-100 scale-100"
-               leaveTo="transform opacity-0 scale-95"
-            >
-               <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <div className="py-1">
-                     {
-                        yearsList().map(year => {
-                           return (<Menu.Item key={year}>
-                              {({ active }) => (
-                                 <span
-                                    key={year}
-                                    onClick={() => props.setCurrentYear(year)}
-                                    className={classNames(
-                                       active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                                       'block px-4 py-2 text-sm'
-                                    )}
-                                 >
-                                    {year}
-                                 </span>
-                              )}
-                           </Menu.Item>
-                           )
-                        })
-                     }
-                  </div>
-               </Menu.Items>
-            </Transition>
-         </Menu> */}
+         <DropDownMenu list={MONTH_NAMES} setCurrent={props.setCurrentMonth} current={props.currentMonth}/>
+         <DropDownMenu list={yearsList()} setCurrent={props.setCurrentYear} current={props.currentYear} />
          <button className='flex items-center gap-6 bg-transparent hover:bg-[#0E5BB5] hover:border-transparent rounded-full'>
             <CalendarRightIcon />
          </button>
