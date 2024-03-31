b535024afd7af5feb970818569382f54b6c8685c
Koz <kenneth.averna@gmail.com>
2024-03-31 16:14:57 -0700

    for development

--- server/config.py
+++ server/config.py
@@ -6,7 +6,8 @@ import redis
 load_dotenv()
 
 class ApplicationConfig:
-   SECRET_KEY = os.environ["SECRET_KEY"]
+   #SECRET_KEY = os.environ["SECRET_KEY"]
+   SECRET_KEY = "3a9a1a0b8da1e2d6edfddfcd60a7c6374b3cb9105ec0aae54e2b723bc04191b6"
 
    SQLALCHEMY_TRACK_MODIFICATIONS = False
    SQLALCHEMY_ECHO = True
@@ -17,5 +18,5 @@ class ApplicationConfig:
 
    SESSION_TYPE = "redis"
    SESSION_PERMANANT = False
-   SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
-   #SESSION_REDIS = redis.from_url("redis://redis:6379/0")
+   #SESSION_REDIS = redis.from_url("redis://127.0.0.1:6379")
+   SESSION_REDIS = redis.from_url("redis://redis:6379/0")
