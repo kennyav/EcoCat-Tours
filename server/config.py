26f10d000d4cd61d8cbebc36c2204b1883f77286
EC2 Default User <ec2-user@ip-172-31-7-53.us-east-2.compute.internal>
2024-04-01 11:55:26 +0000

    from ec2

--- server/config.py
+++ server/config.py
@@ -13,8 +13,8 @@ class ApplicationConfig:
    SQLALCHEMY_ECHO = True
    SQLALCHEMY_DATABASE_URI = r"sqlite:///./db.sqlite"
 
-   SESSION_COOKIE_SAMESITE = "None"
-   SESSION_COOKIE_SECURE = True
+   #SESSION_COOKIE_SAMESITE = "None"
+   #SESSION_COOKIE_SECURE = True
 
    SESSION_TYPE = "redis"
    SESSION_PERMANANT = False
