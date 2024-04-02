59943b4509ed9c983bd4b6caea66fec8c589d879
Koz <kenneth.averna@gmail.com>
2024-04-01 19:37:15 -0700

    for production

--- frontend/src/reducers/developmentSlice.js
+++ frontend/src/reducers/developmentSlice.js
@@ -3,8 +3,8 @@ import { createSlice } from '@reduxjs/toolkit'
 export const developmentSlice = createSlice({
   name: 'development',
   initialState: {
-    //value: "http://3.15.70.119",
-    value: "http://127.0.0.1",
+    value: "http://3.15.70.119",
+    //value: "http://127.0.0.1",
   },
   reducers: {
     update: (state, action) => {
